d598e726c39400491b41ec211226933200ad7a09
Siyue Sun <>
2022-03-26 16:44:53 +0800

    added file extention

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import CoinTable from "./components/CoinTable";
+import CoinTable from "./components/CoinTable.jsx";
 
 function App() {
   return (
     <div style={{ display: "block", padding: 30 }}>
       <h4>Hot coins from Kucoin</h4>
       <CoinTable />
-      {/* <Cointable /> */}
     </div>
   );
 }
