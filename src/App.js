ab43d0d6c0e1ddf114faf2f88fdaf747cbcfae49
Siyue Sun <>
2022-03-26 16:37:26 +0800

    removed typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import Timer from "./components/Cointable";
-
-import CoinTable from "./components/Cointable";
+import CoinTable from "./components/CoinTable";
 
 function App() {
   return (
