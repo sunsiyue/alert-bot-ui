95fa3bb1dc40557acfb16fb951a5839dd58548e4
gokusun <sunsiyue@users.noreply.github.com>
2023-01-09 14:19:01 +0800

    moved binance on top

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     // <div className="bp4-dark">
     <div style={{ padding: 30 }}>
-      <h4>Hot coins from Kucoin</h4>
-      <CoinTable />
       <h4>Hot coins from Binance</h4>
       <BinanceCoinTable />
+      <h4>Hot coins from Kucoin</h4>
+      <CoinTable />
     </div>
     // </div>
   );
